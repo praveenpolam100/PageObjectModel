be47a1c087c82809dd78b1ed88122ea95729bfac
praveenpolam100 <80611426+praveenpolam100@users.noreply.github.com>
2024-05-30 01:14:01 -0500

    updated files for platform remote_git and base class is added with browser support Firefox

--- src/test/java/testclasses/BaseClassTest.java
+++ src/test/java/testclasses/BaseClassTest.java
@@ -2,11 +2,14 @@ package testclasses;
 
 import base.AppConstants;
 import io.github.bonigarcia.wdm.WebDriverManager;
+import io.github.bonigarcia.wdm.managers.FirefoxDriverManager;
 import org.openqa.selenium.*;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.edge.EdgeDriver;
 import org.openqa.selenium.edge.EdgeOptions;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.firefox.FirefoxOptions;
 import org.openqa.selenium.remote.RemoteWebDriver;
 import org.testng.annotations.*;
 import pageobjects.LoginPage;
@@ -24,6 +27,10 @@ public class BaseClassTest {
 
     protected LoginPage loginPage;
 
+    private ChromeOptions chromeOptions;
+    private EdgeOptions edgeOptions;
+    private FirefoxOptions firefoxOptions;
+
 
     @BeforeTest
     @Parameters({"browserName"})
@@ -39,10 +46,10 @@ public class BaseClassTest {
                 driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
 
             }else if (AppConstants.platform.equalsIgnoreCase("remote")){
-                ChromeOptions chromeOptions = new ChromeOptions();
+                chromeOptions = new ChromeOptions();
                 chromeOptions.setPlatformName("linux");
                 chromeOptions.setPageLoadStrategy(PageLoadStrategy.EAGER);
-                URL url = null;
+                URL url;
                 try {
                     //url = new URL("http://localhost:4444/wd/hub");
                     url = new URL("http://localhost:4444/wd/hub");
@@ -51,21 +58,28 @@ public class BaseClassTest {
                 }
                 driver = new RemoteWebDriver(url, chromeOptions);
 
-
+            }else if (AppConstants.platform.equalsIgnoreCase("remote_git")){
+                chromeOptions = new ChromeOptions();
+                chromeOptions.addArguments("--start-maximized");
+                chromeOptions.addArguments("--headless");
+                chromeOptions.addArguments("--disable-gpu");
+                chromeOptions.addArguments("--no-sandbox");
+                WebDriverManager.chromedriver().setup();
+                driver = new ChromeDriver(chromeOptions);
 
             }
         }else if (browserName.equalsIgnoreCase("Edge")) {
                 if (AppConstants.platform.equalsIgnoreCase("local")) {
                     WebDriverManager.edgedriver().setup();
-                    driver = new FirefoxDriver();
+                    driver = new EdgeDriver();
                     driver.manage().window().maximize();
                     driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
                 }
                 else if (AppConstants.platform.equalsIgnoreCase("remote")){
-                EdgeOptions edgeOptions = new EdgeOptions();
+                    edgeOptions = new EdgeOptions();
                     edgeOptions.setPlatformName("linux");
                     edgeOptions.setPageLoadStrategy(PageLoadStrategy.EAGER);
-                URL url = null;
+                URL url;
                 try {
                     //url = new URL("http://localhost:4445/wd/hub");
                     url = new URL("http://localhost:4444/wd/hub");
@@ -74,9 +88,50 @@ public class BaseClassTest {
                 }
                 driver = new RemoteWebDriver(url, edgeOptions);
                 loginPage = new LoginPage(driver);
+                }
+                else if (AppConstants.platform.equalsIgnoreCase("remote_git")) {
+                    edgeOptions = new EdgeOptions();
+                    edgeOptions.addArguments("--start-maximized");
+                    edgeOptions.addArguments("--headless");
+                    edgeOptions.addArguments("--disable-gpu");
+                    edgeOptions.addArguments("--no-sandbox");
+                    WebDriverManager.edgedriver().setup();
+                    driver = new EdgeDriver(edgeOptions);
+
+                }
+        }else if (browserName.equalsIgnoreCase("Firefox")){
+            if (AppConstants.platform.equalsIgnoreCase("local")) {
+                FirefoxDriverManager.firefoxdriver().setup();
+                driver = new FirefoxDriver();
+                driver.manage().window().maximize();
+                driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
+            }
+            else if (AppConstants.platform.equalsIgnoreCase("remote")){
+                firefoxOptions = new FirefoxOptions();
+                firefoxOptions.setPlatformName("linux");
+                firefoxOptions.setPageLoadStrategy(PageLoadStrategy.EAGER);
+                URL url;
+                try {
+                    //url = new URL("http://localhost:4445/wd/hub");
+                    url = new URL("http://localhost:4444/wd/hub");
+                } catch (MalformedURLException e) {
+                    throw new RuntimeException(e);
+                }
+                driver = new RemoteWebDriver(url, firefoxOptions);
+                loginPage = new LoginPage(driver);
+            }
+            else if (AppConstants.platform.equalsIgnoreCase("remote_git")) {
+                firefoxOptions = new FirefoxOptions();
+                firefoxOptions.addArguments("--start-maximized");
+                firefoxOptions.addArguments("--headless");
+                firefoxOptions.addArguments("--disable-gpu");
+                firefoxOptions.addArguments("--no-sandbox");
+                WebDriverManager.firefoxdriver().setup();
+                driver = new FirefoxDriver(firefoxOptions);
+
+            }
 
 
-             }
         }
         else {
                 System.out.println("The supplied browsername is not supported");
